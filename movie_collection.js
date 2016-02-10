ac02496420b04a410a4b527015f4ab0f5db3f7b9
philiptang42 <philiptang42@gmail.com>
2016-02-10 15:56:49 -0500

    finding nemo case

--- movie_collection.js
+++ movie_collection.js
@@ -2,6 +2,9 @@ var getReview = function(movie) {
   switch(movie) {
     case "Toy Story 2":
       return ("Great story. Mean prospector.");
+    case "Finding Nemo":
+      return ("Cool animation, and funny turtles.");
+
   }
 };
 
