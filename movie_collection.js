c8ce5404f36b8558a054b329d918f409d5c6289e
philiptang42 <philiptang42@gmail.com>
2016-02-10 15:56:20 -0500

    switch case ts2

--- movie_collection.js
+++ movie_collection.js
@@ -1,3 +1,8 @@
-var getReview = function (movie) {
-  
+var getReview = function(movie) {
+  switch(movie) {
+    case "Toy Story 2":
+      return ("Great story. Mean prospector.");
+  }
 };
+
+console.log(getReview("Toy Story 2"))
